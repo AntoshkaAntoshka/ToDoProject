554edf48ce7f40e9f8755b8d4a724c827f8dc3a0
AntoshkaAntoshka <antongrishin1994@mail.ru>
2022-05-16 08:59:07 -0700

    Lesson_1 - User_API

---
+++ userworkapp/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class User(models.Model):
+    user_name = models.CharField('Username', max_length=64)
+    first_name = models.CharField('Firstname', max_length=64)
+    last_name = models.CharField('Lastname', max_length=64)
+    email = models.EmailField('E-mail', max_length=100, unique=True)

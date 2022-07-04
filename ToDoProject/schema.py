994265ba633015784ce8a4009cda0cb35b769527
AntoshkaAntoshka <antongrishin1994@mail.ru>
2022-07-04 04:08:23 -0700

    lesson_10.GraphQL_actions

--- ToDoProject/schema.py
+++ ToDoProject/schema.py
@@ -22,11 +22,29 @@ class TODOType(DjangoObjectType):
 class Query(graphene.ObjectType):
     all_users = graphene.List(UserType)
     all_projects = graphene.List(ProjectType)
+    all_todo = graphene.List(TODOType)
+    todo_by_id = graphene.Field(TODOType, id=graphene.Int(required=True))
+    project_by_name = graphene.List(ProjectType, name=graphene.String(required=False))
 
     def resolve_all_users(root, info):
         return User.objects.all()
 
     def resolve_all_projects(root, info):
         return Project.objects.all()
+
+    def resolve_all_todo(root, info):
+        return TODO.objects.all()
+
+    def resolve_todo_by_id(self, info, id):
+        try:
+            return TODO.objects.get(id=id)
+        except TODO.DoesNotExist:
+            return None
+
+    def resolve_project_by_name(self, info, name=None):
+        projects = Project.objects.all()
+        if name:
+            projects = projects.filter(project_authors__first_name=name)
+        return projects
 
 schema = graphene.Schema(query=Query)
